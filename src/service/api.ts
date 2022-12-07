92d080747e9f4b28019bbed6c5c9da918d3e0e99
aledale <ale.salas271102@gmail.com>
2022-12-06 23:59:37 -0300

    adding  quiz types

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,12 +1,31 @@
+import { shuffleArray } from './utils'
+
+
 export enum Difficulty {
     EASY = 'easy',
     MEDIUM = 'medium',
     HARD = 'hard'
 }
 
+export type Question = {
+    category: string,
+    correct_answer: string,
+    difficulty: string,
+    incorrect_answer: string[],
+    question:string,
+    type:string
+}
+
+export type QuestionState = Question & {answer: string[]}
+
 export const fetchQuestions = async (amount: number, difficulty: Difficulty) => {
     const endPoint = `https://opentdb.com/api.php?amount=${amount}&category=9&difficulty=${difficulty}&type=multiple`
     const data = await (await fetch(endPoint)).json()
     //const json = await data.json()
-    console.log(data);
+    return data.results.map((question:Question) => (
+        {
+            ...question,
+            answer: shuffleArray([...question.incorrect_answer, question.correct_answer])
+        }
+    ))
 }
